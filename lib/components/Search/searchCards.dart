9c7c5dc7a287bba57845c60e1e7f3b7fedab0804
Gabe239 <gurbinaia@gmail.com>
2024-04-14 01:24:36 -0400

    Deleted photos from searchCards

--- lib/components/Search/searchCards.dart
+++ lib/components/Search/searchCards.dart
@@ -14,22 +14,18 @@ class SearchCards extends StatelessWidget {
     List<Map<String, dynamic>> cardData = [
       {
         'name': 'Rock',
-        'imagePath': 'assets/images/albums/album18.jpeg',
         'color': Colors.purpleAccent,
       },
       {
         'name': 'English',
-        'imagePath': 'assets/images/albums/album12.jpg',
         'color': Colors.blueAccent,
       },
       {
         'name': 'Spanish',
-        'imagePath': 'assets/images/albums/album14.jpeg',
         'color': Colors.greenAccent,
       },
       {
         'name': 'French',
-        'imagePath': 'assets/images/albums/album16.png',
         'color': Colors.orangeAccent,
       },
     ];
@@ -43,7 +39,6 @@ class SearchCards extends StatelessWidget {
           Expanded(
             child: CardGenre(
               name: cardData[i]['name'],
-              imagePath: cardData[i]['imagePath'],
               backgroundColor: cardData[i]['color'],
             ),
           ),
@@ -54,7 +49,6 @@ class SearchCards extends StatelessWidget {
           Expanded(
             child: CardGenre(
               name: cardData[i + 1]['name'],
-              imagePath: cardData[i + 1]['imagePath'],
               backgroundColor: cardData[i + 1]['color'],
             ),
           ),
@@ -78,13 +72,11 @@ class SearchCards extends StatelessWidget {
 
 class CardGenre extends StatelessWidget {
   final String name;
-  final String imagePath;
   final Color backgroundColor;
 
   const CardGenre({
     Key? key,
     required this.name,
-    required this.imagePath,
     required this.backgroundColor,
   }) : super(key: key);
 
@@ -124,12 +116,6 @@ class CardGenre extends StatelessWidget {
                   ],
                 ),
               ),
-              Expanded(
-                child: DiamondClip(
-                  imagePath: imagePath,
-                  size: 100.0,
-                ),
-              ),
             ],
           ),
         ),
@@ -138,49 +124,3 @@ class CardGenre extends StatelessWidget {
   }
 }
 
-class DiamondClip extends StatelessWidget {
-  final String imagePath;
-  final double size;
-
-  const DiamondClip({
-    Key? key,
-    required this.imagePath,
-    required this.size,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ClipPath(
-      clipper: DiamondClipper(),
-      child: Container(
-        width: size,
-        height: size,
-        child: Image.asset(
-          imagePath,
-          fit: BoxFit.cover,
-        ),
-      ),
-    );
-  }
-}
-
-class DiamondClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    final path = Path();
-
-    // Define the diamond shape using path commands
-    path.moveTo(size.width / 2, 0); // Top point
-    path.lineTo(size.width, size.height / 2); // Right point
-    path.lineTo(size.width / 2, size.height); // Bottom point
-    path.lineTo(0, size.height / 2); // Left point
-    path.close(); // Close the path to form a complete shape
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return false;
-  }
-}
